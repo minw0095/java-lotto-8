a0bbaa759eb39256cf4acc75556dc058003368f9
minw0095 <kmu6898@naver.com>
2025-11-03 19:16:22 +0900

    feat: 당첨 상금에 대한 기능

---
+++ src/main/java/lotto/Prize.java
@@ -0,0 +1,68 @@
+package lotto;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public enum Prize {
+    FIFTH(3, 5_000, false, 0),
+    FOURTH(4, 50_000, false, 0),
+    THIRD(5, 1_500_000, false, 0),
+    SECOND(5, 30_000_000, true, 0),
+    FIRST(6, 2_000_000_000, false, 0),
+    ;
+
+    private final int matchCount;
+    private final int reward;
+    private final boolean isMatchBonus;
+    private int winnerCount;
+
+    Prize(int matchCount, int reward, boolean isMatchBonus, int winnerCount) {
+        this.matchCount = matchCount;
+        this.reward = reward;
+        this.isMatchBonus = isMatchBonus;
+        this.winnerCount = winnerCount;
+    }
+
+    public static Prize findMatchCount(long matchNumber, boolean bonus) {
+        return Arrays.stream(values())
+                .filter(e -> e.matchCount == matchNumber && e.isMatchBonus == bonus)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static void check(long matchNumber, boolean bonus) {
+        if (findMatchCount(matchNumber, bonus) != null) {
+            findMatchCount(matchNumber, bonus).winnerCount += 1;
+        }
+
+    }
+
+    public int getWinnerCount() {
+        return winnerCount;
+    }
+
+    public int getTotalReward() {
+        return reward * winnerCount;
+    }
+
+    private String makeComma(int number) {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        return decimalFormat.format(number);
+    }
+
+    public void printResult() {
+        if (this == SECOND) {
+            System.out.println(matchCount + "개 일치, 보너스 볼 일치 " + "(" + makeComma(reward) + "원) - " + winnerCount + "개");
+            return;
+        }
+        System.out.println(matchCount + "개 일치 " + "(" + makeComma(reward) + "원) - " + winnerCount + "개");
+    }
+
+    public static double totalReward() {
+        return Arrays.stream(values()).mapToInt(Prize::getTotalReward).sum();
+    }
+
+    public static String getRateOfReturn(int money) {
+        return String.format("%.1f", totalReward() * 100 / money);
+    }
+}
