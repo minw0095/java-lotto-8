d057b9ab0c9dad776624f325128a0540102fed6d
minw0095 <kmu6898@naver.com>
2025-11-03 15:41:28 +0900

    feat: 입력 받기

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String moneyInput() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String lottoInput() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String bonusInput() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+}
